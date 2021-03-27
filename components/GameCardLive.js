cb712a0d3853d8e9f686bfbb943151533c54d924
Noah Vaillancourt <noah.vaillancourt@gmail.com>
2021-03-26 23:08:26 -0400

    implemented server side rendering to make things a bit smoother on page load
    going to work on setting up some kind of skeletion loading thing if data is present yet just to avoid layout changes

--- components/GameCardLive.js
+++ components/GameCardLive.js
@@ -7,7 +7,7 @@ export default function GameCardLive({gameData}){
     const awayteamimage = `https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/${gameData.teams.away.team.id}.svg`
 
     return (
-        <div className="relative md:p-2 flex divide-y-1 w-full md:rounded-md divide-solid divide-white p-2 pr-4 mb-2 md:pr-4 border-b justify-between items-center">
+        <div className="relative md:p-2 flex divide-y-1 w-full md:rounded-md divide-solid divide-white p-2 pr-4 mb-2 md:pr-4 md:mr-4 md:w-80 md:border-0 md:bg-black border-b justify-between items-center md:mb-4">
             <div className="rounded-full bg-red-500 top-0 rightc-0 absolute h-5 w-5 m-2 animate-pulse"/>
             <div className="flex flex-col w-full">
                 <TeamDetails teamInfo={gameData.teams.away} isLive={true} isScheduled={false} type="away"/>
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,15 @@
 import Head from 'next/head'
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import dayjs from 'dayjs';
-import useSWR from 'swr';
 import GameCardScheduled from '../components/GameCardScheduled'
 import GameCardFinished from '../components/GameCardFinished'
 import GameCardLive from '../components/GameCardLive'
-import "react-datepicker/dist/react-datepicker.css";
 
-
-function GameDisplay({date}) {
-
-  //sets up how the data will be returned
-  const fetcher = (...args) => fetch(...args).then(res => res.json())
-  //takes the date and formats it so it can be used in an API call
-  const dateFormattedForAPI = dayjs(date).format("YYYY-MM-DD")
-
-  //calls the NHL API and gets the schedule for the day
-  const {data, error} = useSWR('https://statsapi.web.nhl.com/api/v1/schedule?date='+dateFormattedForAPI, fetcher)
-
-  if(!data) return <p>loading</p>
-  if(error) return <p>error</p>
+function GameDisplay({data}) {
   return (
     <div className="flex flex-wrap justify-start flex-start content-start md:ml-2">
       {
-        data.dates[0].games.map((game) => {
+        data.games.map((game) => {
           if(game.status.statusCode === "1" || game.status.statusCode === "2"){
             return <GameCardScheduled key={data.gamePk} data={game}/>
           }
@@ -39,28 +25,33 @@ function GameDisplay({date}) {
   )
 }
 
-export default function Home() {
-
-  //creates an array for use with dayjs.add()
-  let navigationDateArray = [0, -1, -2, -3, -4, -5]
-
-  //loops over each item in the above array and returns a formatted date for use with the tab navigation
-  let navigationDateFormatted = navigationDateArray.map((date) => {
-    return dayjs().add(date, 'day').format('MMMM D, YYYY')
-  })
-  
+function Home(props) {
   return (
     <div>
       {
-        navigationDateFormatted.map((date) => {
+        props.data.map((date, index) => {
           return <div>
             <div className="bg-white block ml-1.5 md:mb-3 md:ml-2">
-              <p className="text-4xl font-semibold" >{dayjs(date).format('MMMM D, YYYY')}</p>
+              <p className="text-4xl font-semibold" >{dayjs(date.date).format('MMMM D, YYYY')}</p>
             </div>
-            <GameDisplay date={date}/>
+            <GameDisplay data={props.data[index]}/>
           </div>
         })
       }
     </div>
   )
 }
+
+export async function getServerSideProps(context){
+  let datesForRequest = [0, -5]
+
+  let datesForRequestFormatted = datesForRequest.map((date) => {
+    return dayjs().add(date, 'day').format('YYYY-MM-DD')
+  })
+  const res = await fetch('https://statsapi.web.nhl.com/api/v1/schedule?startDate='+datesForRequestFormatted[1]+'&endDate='+datesForRequestFormatted[0])
+  const data = await res.json()
+
+  return {props: {data: data.dates.reverse()}}
+}
+
+export default Home
