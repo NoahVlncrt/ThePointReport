20b64b407aa73d4fa2de9f105369a4fa8daba789
Noah Vaillancourt <noah.vaillancourt@gmail.com>
2021-05-12 20:11:26 -0400

    it now properly displays if there wasn't a play
    looking to add hits and shots on goal and stuff at some point

--- components/GameDetails/ScoringEvent.js
+++ components/GameDetails/ScoringEvent.js
@@ -1,36 +1,41 @@
 import Image from 'next/image'
 
 export default function ScoringEvent(props){
-    const teamLogo = `https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/${props.playInfo.team.id}.svg`
+    const teamLogo = `https://www-league.nhlstatic.com/images/logos/teams-current-primary-light/${props.playInfo.team}.svg`
     let playerPhoto = `https://cms.nhl.bamgrid.com/images/headshots/current/60x60/${props.playInfo.players[0].player.id}@2x.jpg`
     return (
-        <div className="flex w-full mt-2 mb-2">
-            <div>
-                <Image
-                    src={teamLogo}
-                    width={60}
-                    height={60}
-                    alt="team logo"
-                />
+        <div className="flex w-full mt-2 mb-2 items-center">
+            <div className="flex w-1/3 items-center">
+                <div>
+                    <Image
+                        src={teamLogo}
+                        width={60}
+                        height={60}
+                        alt="team logo"
+                    />
+                </div>
+                <p>{props.playInfo.time}</p>
             </div>
-            <p>{props.playInfo.about.periodTime}</p>
-            <div className="rounded-full">
-                <Image
-                    src={playerPhoto}
-                    width={60}
-                    height={60}
-                    alt="yeet "
-                />
-            </div>
-            <div className="flex flex-col">
-                {props.playInfo.players.map((player, index) => {
-                    if(index === 0){
-                        return <p className="font-bold">{player.player.fullName} ({player.seasonTotal})</p>
-                    }
-                    if(player.playerType != 'Goalie'){
-                        return <p>{player.player.fullName} ({player.seasonTotal})</p>
-                    }
-                })}
+
+            <div className="flex flex-row w-2/3">
+                <div className="rounded-full">
+                    <Image
+                        src={playerPhoto}
+                        width={60}
+                        height={60}
+                        alt="yeet "
+                    />
+                </div>
+                <div className="flex flex-col">
+                    {props.playInfo.players.map((player, index) => {
+                        if(index === 0){
+                            return <p className="font-bold">{player.player.fullName} ({player.seasonTotal})</p>
+                        }
+                        if(player.playerType != 'Goalie'){
+                            return <p>{player.player.fullName} ({player.seasonTotal})</p>
+                        }
+                    })}
+                </div>      
             </div>
         </div>
     )
--- pages/game/[gameid].js
+++ pages/game/[gameid].js
@@ -3,15 +3,13 @@ import Image from 'next/image'
 import ScoringEvent from '../../components/GameDetails/ScoringEvent'
 
 function Game(props){
-
-    console.log(props.data.periods)
     return (
         <div className="ml-2 mr-2">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M7 16l-4-4m0 0l4-4m-4 4h18" />
             </svg>
             <p className="text-xl">{dayjs(props.data.gameDate).format('MMMM D, YYYY')}</p>
-            <div className="flex justify-between items-center">
+            <div className="flex justify-around items-center">
                 <div className="flex items-center flex-col-reverse">
                     <div className="flex flex-col justify-center">
                         <p>{props.data.away.abbreviation}</p>
@@ -45,28 +43,24 @@ function Game(props){
             </div>
             <div className="flex flex-col justify-center items-center">
                 {props.data.periods.map((period, index) => {
+                    let ScoringPlays = props.data.scoringPlays.filter(play => play.period === index+1)
+                    if(ScoringPlays.length === 0 ){
+                        return <div className="w-full">
+                            <p className="font-bold text-xl">Period {index + 1}</p>
+                            <div>
+                                <p>no play</p>
+                            </div>
+                        </div>
+                    }
                     return <div className="w-full">
                         <p className="font-bold text-xl">Period {index +1}</p>
-                        <div id="period">
-                            {props.data.scoringPlays.map((play, index2) => {
-                                if(play.about.period === index +1){
-                                    return <ScoringEvent playInfo={play}/>
-                                }
-                            })}
+                        <div className="flex flex-col">
+                            {ScoringPlays.map((player) => {
+                                return <ScoringEvent playInfo={player}/>
+                            }).reverse()}
                         </div>
-                        {
-                            props.data.scoringPlays.map((play, index2) => {
-                                if(play.about.period != index -1 && play.about.period != index +1 && play.about.period === index && play.about.period != index +2 && play.about.period != index -2 && index2 <= 0){
-                                    return <p> no play</p>
-                                }
-                            })
-                        }
-                        
                     </div>
                 })}
-                {/* {props.data.scoringPlays.map((play) => {
-                    return <ScoringEvent playInfo={play}/>
-                })} */}
             </div>
         </div>
     )
@@ -78,15 +72,16 @@ export async function getServerSideProps(context){
     const data = await res.json()
 
     let scoringPlays = data.liveData.plays.scoringPlays.map((play) => {
-        return data.liveData.plays.allPlays[play]
+        return { period: data.liveData.plays.allPlays[play].about.period, players: data.liveData.plays.allPlays[play].players, time: data.liveData.plays.allPlays[play].about.periodTime, team: data.liveData.plays.allPlays[play].team.id}
     })
 
     let periodArray = new Array(data.liveData.linescore.currentPeriod)
 
+
     let sortedData = {
         gameDate: data.gameData.datetime.dateTime,
         periods: periodArray,
-        scoringPlays,
+        scoringPlays: scoringPlays.reverse(),
         away: {
             abbreviation: data.gameData.teams.away.abbreviation,
             score: data.liveData.linescore.teams.away.goals,
