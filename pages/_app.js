4c330c7c485a6113e92dea3b17b09053b462b54e
Noah Vaillancourt <noah.vaillancourt@gmail.com>
2023-10-19 22:40:57 -0400

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
-
 import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} className="debug-screens bg-gray-200" />
+  return <>
+    <Component {...pageProps} className="debug-screens bg-gray-200" />
+    <Analytics/>
+  </>
 }
 
 export default MyApp
